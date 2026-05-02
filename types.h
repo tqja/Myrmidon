c71e8d267f7c3d1517e605ee0d6b254ac9387748
tqja <1tariqameer@gmail.com>
2026-05-02 17:41:31 +0100

    Define types for Position class

---
+++ types.h
@@ -0,0 +1,130 @@
+#ifndef CHESS_ENGINE_TYPES_H
+#define CHESS_ENGINE_TYPES_H
+#include <cassert>
+#include <cctype>
+#include <cstdint>
+#include <iostream>
+#include <ostream>
+
+using Bitboard = std::uint64_t;
+
+constexpr char piece_chars[2][7] = {
+    {'P', 'N', 'B', 'R', 'Q', 'K', '.'}, // white
+    {'p', 'n', 'b', 'r', 'q', 'k', '.'}, // black
+};
+
+enum Colour : std::uint8_t { WHITE, BLACK, COLOUR_NB };
+
+// clang-format off
+enum PieceType : std::uint8_t {
+  PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING,
+  PIECE_TYPE_NB,
+  NO_PIECE_TYPE = PIECE_TYPE_NB
+};
+// clang-format on
+
+inline PieceType getPieceType(char c) {
+  switch (static_cast<char>(std::tolower(c))) {
+  case 'p':
+    return PAWN;
+  case 'n':
+    return KNIGHT;
+  case 'b':
+    return BISHOP;
+  case 'r':
+    return ROOK;
+  case 'q':
+    return QUEEN;
+  case 'k':
+    return KING;
+  default:
+    std::cerr << c << " is not a valid piece type." << std::endl;
+    assert(false);
+  }
+}
+
+// clang-format off
+enum Square : std::uint8_t {
+  SQ_A1, SQ_B1, SQ_C1, SQ_D1, SQ_E1, SQ_F1, SQ_G1, SQ_H1,
+  SQ_A2, SQ_B2, SQ_C2, SQ_D2, SQ_E2, SQ_F2, SQ_G2, SQ_H2,
+  SQ_A3, SQ_B3, SQ_C3, SQ_D3, SQ_E3, SQ_F3, SQ_G3, SQ_H3,
+  SQ_A4, SQ_B4, SQ_C4, SQ_D4, SQ_E4, SQ_F4, SQ_G4, SQ_H4,
+  SQ_A5, SQ_B5, SQ_C5, SQ_D5, SQ_E5, SQ_F5, SQ_G5, SQ_H5,
+  SQ_A6, SQ_B6, SQ_C6, SQ_D6, SQ_E6, SQ_F6, SQ_G6, SQ_H6,
+  SQ_A7, SQ_B7, SQ_C7, SQ_D7, SQ_E7, SQ_F7, SQ_G7, SQ_H7,
+  SQ_A8, SQ_B8, SQ_C8, SQ_D8, SQ_E8, SQ_F8, SQ_G8, SQ_H8,
+  SQ_NONE,
+
+  SQ_ZERO = 0,
+  SQ_NB = 64
+};
+// clang-format on
+
+enum File : std::uint8_t {
+  FILE_A,
+  FILE_B,
+  FILE_C,
+  FILE_D,
+  FILE_E,
+  FILE_F,
+  FILE_G,
+  FILE_H,
+  FILE_NB
+};
+
+enum Rank : std::uint8_t {
+  RANK_1,
+  RANK_2,
+  RANK_3,
+  RANK_4,
+  RANK_5,
+  RANK_6,
+  RANK_7,
+  RANK_8,
+  RANK_NB
+};
+
+#define ENABLE_INCR_OPERATORS_ON(T)                                            \
+  constexpr T &operator++(T &d) {                                              \
+    return d = static_cast<T>(static_cast<int>(d) + 1);                        \
+  }                                                                            \
+  constexpr T &operator--(T &d) {                                              \
+    return d = static_cast<T>(static_cast<int>(d) - 1);                        \
+  }
+
+ENABLE_INCR_OPERATORS_ON(PieceType)
+ENABLE_INCR_OPERATORS_ON(Square)
+ENABLE_INCR_OPERATORS_ON(File)
+ENABLE_INCR_OPERATORS_ON(Rank)
+
+#undef ENABLE_INCR_OPERATORS_ON
+
+enum Direction : std::int8_t {
+  NORTH = 8,
+  EAST = 1,
+  SOUTH = -NORTH,
+  WEST = -EAST,
+
+  NORTH_EAST = NORTH + EAST,
+  SOUTH_EAST = SOUTH + EAST,
+  SOUTH_WEST = SOUTH + WEST,
+  NORTH_WEST = NORTH + WEST
+};
+
+enum CastlingRights : std::uint8_t {
+  NO_CASTLING,
+  WHITE_OO,
+  WHITE_OOO = WHITE_OO << 1,
+  BLACK_OO = WHITE_OO << 2,
+  BLACK_OOO = WHITE_OO << 3,
+
+  KING_SIDE = WHITE_OO | BLACK_OO,
+  QUEEN_SIDE = WHITE_OOO | BLACK_OOO,
+  WHITE_CASTLING = WHITE_OO | WHITE_OOO,
+  BLACK_CASTLING = BLACK_OO | BLACK_OOO,
+  ANY_CASTLING = WHITE_CASTLING | BLACK_CASTLING,
+
+  CASTLING_RIGHT_NB = 16
+};
+
+#endif // CHESS_ENGINE_TYPES_H
