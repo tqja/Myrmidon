cf20a4789da0e55b04f239e3bd0078857378dcc7
tqja <1tariqameer@gmail.com>
2026-05-12 05:35:29 +0100

    Output move info for debug

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include "Position.h"
 #include "Zobrist.h"
+#include "Move.h"
 
 int main() {
     Position board{};
     board.set("rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2");
     board.print();
 
-    Zobrist z{};
-    std::cout << z.compute(board);
-    std::cout << board.fen();
+    const Zobrist z{};
+    std::cout << "Hash:              " << z.compute(board) << '\n';
+    std::cout << board.fen() << '\n';
+    constexpr Move m{SQ_E4, SQ_E6, QUEEN_CASTLE};
+    std::cout << static_cast<int>(m.from()) << " " << static_cast<int>(m.to()) << " " << m.flags() << '\n';
     return 0;
 }
